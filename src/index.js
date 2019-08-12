12ee99ba568abbe328a30a747c1cc939ef947812
Jonathan Lassen <jonathan.lassen@gmail.com>
2019-08-12 12:07:02 -0400

    building out routes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,33 @@ import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
-    <section >
-      <main className="main">
-        <MapLeaflet />
-      </main>
-    </section>
+    <div>
+      <nav className="flex items-center justify-between flex-wrap bg-blue-300 p-6">
+         <div className="flex items-center flex-no-shrink text-white mr-6">
+            <span className="font-semibold text-xl tracking-tight">Find a Frameshop</span>
+        </div>
+      
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm flex-grow">
+            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-blue-lighter hover:text-white mr-4">
+              Autocomplete here
+            </a>  
+          </div>
+          <div>
+            <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue hover:bg-white mt-4 mr-2 lg:mt-0">Login</a>  
+            <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue hover:bg-white mt-4 lg:mt-0">Register</a>
+
+            </div>
+          </div> 
+      </nav>
+
+
+      <section >
+        <main className="main">
+          <MapLeaflet />
+        </main>
+      </section>
+    </div>
   );
 }
 
